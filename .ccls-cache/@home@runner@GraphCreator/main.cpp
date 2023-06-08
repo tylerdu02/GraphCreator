a4e2b723243cd6ee70e61d520f42ee91e26d7ad6
tylerdu02 <19107265-tylerdu02@users.noreply.replit.com>
2023-06-08 14:43:03 +0000

    3rd

--- .ccls-cache/@home@runner@GraphCreator/main.cpp
+++ .ccls-cache/@home@runner@GraphCreator/main.cpp
@@ -103,3 +103,99 @@ void removeVertex(Node** List, int graphTable[21][21]) { // removes vertex
         graphTable[index][i] = 0;
   }
 }
+
+void addEdge(Node** List, int graphTable[21][21]) { // adds edge 
+    char* name1 = new char[4];
+    char* name2 = new char[4];
+    int edgeWeight;
+    cout << "What is the label name of your FIRST vertex?" << endl;
+    cin.get(name1, 4);
+    cin.ignore(1, '\n');
+    cout << "What is the label name of your SECOND vertex?" << endl;
+    cin.get(name2, 4);
+    cin.ignore(1, '\n');
+    cout << "What the weight of the edge?" << endl;
+    cin >> edgeWeight;
+    cin.ignore(1, '\n');
+
+    int index1 = 398121;
+    int index2 = 398121;
+    for (int i = 0; i < 21; i++) {
+        if (List[i] != NULL) {
+            if (strcmp(List[i]->name, name1) == 0) {
+                index1 = i;
+            }
+            else if (strcmp(List[i]->name, name2) == 0) {
+                index2 = i;
+            }
+        }
+    }
+
+    if (index1 != 398121 && index2 != 398121) { // adds edge weight
+        graphTable[index1][index2] = edgeWeight;
+        graphTable[index2][index1] = edgeWeight;
+    }
+    else {
+        cout << "Invalid input. Try again." << endl;
+    }
+}
+
+void removeEdge(Node** List, int graphTable[21][21]) { // removes edge
+    char* name1 = new char[4];
+    char* name2 = new char[4];
+    cout << "What is the label name of the FIRST vertex?" << endl;
+    cin.get(name1, 4);
+    cin.ignore(1, '\n');
+    cout << "What is the label name of your SECOND vertex?" << endl;
+    cin.get(name2, 4);
+    cin.ignore(1, '\n');
+
+    int index1 = 398121;
+    int index2 = 398121;
+    for (int i = 0; i < 21; i++) { 
+        if (List[i] != NULL) {
+            if (strcmp(List[i]->name, name1) == 0) {
+                cout << "Edge has been removed" << endl;
+                index1 = i;
+            }
+            else if (strcmp(List[i]->name, name2) == 0) {
+                cout << "Edge has been removed" << endl;
+                index2 = i;
+            }
+        }
+    }
+
+    if (index1 != 398121 && index2 != 398121) { // removes the edge weight 
+        graphTable[index1][index2] = 0;
+        graphTable[index2][index1] = 0;
+    }
+    else {
+        cout << "Invalid input. Try again." << endl;
+    }
+}
+
+void print(int graphTable[21][21]) { // prints 
+    cout << "   ";
+    for (int i = 0; i < 21; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < 21; i++) {
+        if (i < 10) {
+            cout << i << "  ";
+        }
+        else {
+            cout << i << " ";
+        }
+        for (int j = 0; j < 21; j++) {
+            if (j < 9) {
+                cout << graphTable[i][j] << " ";
+            }
+            else {
+                cout << graphTable[i][j] << "  ";
+            }
+        }
+        cout << endl;
+    }
+}
