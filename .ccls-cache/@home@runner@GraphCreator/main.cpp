88d6da7bcbf43cf20c0a1fee8ace9a86c2b647d6
tylerdu02 <19107265-tylerdu02@users.noreply.replit.com>
2023-06-08 13:53:25 +0000

    1st

--- .ccls-cache/@home@runner@GraphCreator/main.cpp
+++ .ccls-cache/@home@runner@GraphCreator/main.cpp
@@ -1,5 +1,25 @@
+/*
+* Tyler Du
+* 6/7/23
+* Graph Creator
+*/
+
 #include <iostream>
+#include <cstring>
+
+struct Node{
+    char* name;
+    Node(char* newName) {
+        name = newName;
+    }
+};
+
+void addNode(Node** List, int graphTable[21][21]);
+void remNode(Node** List, int graphTable[21][21]);
+void addEdge(Node** List, int graphTable[21][21]);
+void remEdge(Node** List, int graphTable[21][21]);
+void print(int graphTable[21][21]);
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
-}
